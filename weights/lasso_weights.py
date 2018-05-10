e3ffc5545bbe179b4e8a3e4a98f777bf71dee923
Dennis Trujillo <dptrujillo@trident.local>
2018-05-09 23:58:20 -0600

    fixed lasso_weights.py

--- weights/lasso_weights.py
+++ weights/lasso_weights.py
@@ -30,60 +30,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2,random_s
 #print(X_test) 
 #print(y_test)
 
+# fit total set to a lasso model 
+alpha=0.1
+print(alpha)
+lasso=Lasso(alpha=alpha,normalize=True)
+lasso.fit(X_train,y_train)
+print('####lasso coeff####')
+print('Model weights:')
+print(lasso.coef_)
 
-# Set the parameters by cross-validation
-tuned_parameters = [{'normalize':[True],'alpha': [1e-4,1e-3,1e-2,1e-1,0,1e1,1e2,1e3]}]
-scores = ['neg_mean_absolute_error']
-
-for score in scores:
-#    print(kernel[i])
-    print("# Tuning hyper-parameters for %s" % score)
-    print()
-
-    clf = GridSearchCV(Lasso(), tuned_parameters, cv=4, verbose=10,n_jobs=4, scoring='%s' % score)
-    clf.fit(X_train, y_train)
-
-    print("Best parameters set found on development set:")
-    print()
-    print(clf.best_params_)
-    print()
-    print('Score:')
-    print(-clf.best_score_)
-    print()
-
-    #build models     
-    model_test=clf.predict(X_test)
-    model_train=clf.predict(X_train)
-    r2_score_train=r2_score(y_train,model_train)
-    mse_score_train=mean_squared_error(y_train,model_train)
-    rmse_score_train=np.sqrt(mse_score_train)
-    r2_score_test=r2_score(y_test,model_test)
-    mse_score_test=mean_squared_error(y_test,model_test)
-    rmse_score_test=np.sqrt(mse_score_test)
-
-    #print('dft test')
-    df1=pd.DataFrame(y_train)
-    df2=pd.DataFrame(np.array(y_train)/model_train)
-    #print(y_test)
-    #print('predicted test') 
-    #print(np.transpose(model_test))
-    #print('dft train')
-    #print(y_train)
-    #print('predicted train')
-    #print(np.transpose(model_train))
-
-    df1.to_csv('lasso_out_'+score+'_optimized.csv')
-    df2.to_csv('lasso_out_'+score+'_optimized.csv',mode='a')
-
-    #plot figures
-    plt.figure() 
-    #plt.title('ML Performed via MLP on Training Set')
-    plt.text(1.85,0.3,'Train:\nR2:%.3f \nMSE:%.3f\nRMSE:%.3f\nTest:\nR2:%.3f\nMSE:%.3f\nRMSE:%.3f' %(r2_score_train,mse_score_train,rmse_score_train,r2_score_test,mse_score_test,rmse_score_test), style='italic', bbox={'facecolor':'red', 'alpha':0.5, 'pad':10},fontsize=10) 
-    plt.plot(y_train,model_train,'gs')
-    plt.plot(y_train,y_train,'k-')
-    plt.plot(y_test,model_test,'ro')
-    plt.plot(y_test,y_test,'k-')
-    #plt.axis([0,1.5,0,1.5])
-    plt.xlabel('True')
-    plt.ylabel('Model')
-    plt.savefig('lasso_train_test'+score+'_optimized.png')
