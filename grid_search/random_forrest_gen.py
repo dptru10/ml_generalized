4d540f6d2234cfd36e93568111f349b1c8374fb2
Dennis Trujillo <dptrujillo@trident.local>
2018-05-10 20:21:12 -0600

    added Agg backend to random_forrest_grid_search

--- grid_search/random_forrest_gen.py
+++ grid_search/random_forrest_gen.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python 
+from __future__ import print_function
+
+import matplotlib 
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np  
 from sklearn.model_selection import GridSearchCV
