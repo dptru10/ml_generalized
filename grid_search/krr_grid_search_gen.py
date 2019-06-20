d4b50e93c0bee4aa57e63086cab0df7d2967d6fb
Dennis Trujillo <dptru10@gmail.com>
2019-06-20 13:53:47 -0400

    added stuff for testing grid vs. randomized hyperparameter search

--- grid_search/krr_grid_search_gen.py
+++ grid_search/krr_grid_search_gen.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python 
 from __future__ import print_function
 
+import time
+import argparse 
 import matplotlib 
+import numpy as np  
+import pandas as pd 
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import numpy as np  
 from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
-from sklearn.metrics import mean_squared_error, r2_score 
 from sklearn.kernel_ridge import KernelRidge
-import pandas as pd 
-import argparse 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score 
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 
 parser= argparse.ArgumentParser()
 parser.add_argument("file1", help="file to be read in", type=str)
+parser.add_argument("--random", action='store_true', default=False)
+parser.add_argument("--grid", action='store_true', default=False)
 args  = parser.parse_args() 
 
 data=pd.read_csv(args.file1)
@@ -32,7 +35,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2,random_s
 
 
 # Set the parameters by cross-validation
-tuned_parameters = [{'kernel':['rbf'],'coef0':[1e-4,1e-3, 1e-2, 1e2, 1e3],'gamma': [1e-4,1e-3, 1e-2, 1e2, 1e3],'alpha': [1e-3,1e-2,1e-1,0,1e1,1e2,1e3]}]
+tuned_parameters = {'kernel':['rbf'],'coef0':[1e-4,1e-3, 1e-2, 1e2, 1e3],'gamma': [1e-4,1e-3, 1e-2, 1e2, 1e3],'alpha': [1e-3,1e-2,1e-1,0,1e1,1e2,1e3]}
 scores = ['neg_mean_absolute_error']
 model='KRR'
 for score in scores:
@@ -40,16 +43,38 @@ for score in scores:
     print("# Tuning hyper-parameters for %s" % score)
     print()
 
-    clf = RandomizedSearchCV(KernelRidge(), tuned_parameters, cv=5, verbose=10,n_jobs=-1, scoring='%s' % score)
-    clf.fit(X_train, y_train)
+    if args.random==True:     
+    	clf = RandomizedSearchCV(KernelRidge(), tuned_parameters, cv=5, verbose=10,n_jobs=-1, scoring='%s' % score)
+    	start_rn=time.time()
+    	clf.fit(X_train,y_train)
+    	end_rn=time.time()
 
-    print("Best parameters set found on development set:")
-    print()
-    print(clf.best_params_)
-    print()
-    print('Score:')
-    print(-clf.best_score_)
-    print()
+    if args.grid==True: 
+    	clf = GridSearchCV(KernelRidge(), tuned_parameters, cv=5, verbose=10,n_jobs=-1, scoring='%s' % score)
+    	start_gr=time.time()
+    	clf.fit(X_train,y_train)
+    	end_gr=time.time()
+
+    	print('GridSearchCV took %.2f seconds for %d candidates parameter settings.' % ((end_gr - start_gr), len(tuned_parameters)))
+
+    	print("Best parameters set (GridSearch) found on development set:")
+    	print()
+    	print(clf.best_params_)
+    	print()
+    	print('Score:')
+    	print(-clf.best_score_)
+    	print()
+
+
+    if args.random==True:
+    	print('RandomizedSearchCV took %.2f seconds for %d candidates parameter settings.' % ((end_rn - start_rn), len(tuned_parameters)))
+    	print("Best parameters set (RandomizedSearch) found on development set:")
+    	print()
+    	print(clf.best_params_)
+    	print()
+    	print('Score:')
+    	print(-clf.best_score_)
+    	print()
 
     #build models     
     model_test=clf.predict(X_test)
@@ -86,7 +111,7 @@ for score in scores:
     #plot figures
     plt.figure() 
     plt.title('ML Performed via %s on Training Set'%model)
-    plt.text(np.max(y_train)-2.2*np.std(y_train),np.min(y_train)+(np.std(y_train)),'Train:\nR2:%.3f \nMSE:%.3f\nRMSE:%.3f\nTest:\nR2:%.3f\nMSE:%.3f\nRMSE:%.3f' %(r2_score_train,mse_score_train,rmse_score_train,r2_score_test,mse_score_test,rmse_score_test), style='italic', bbox={'facecolor':'red', 'alpha':0.5, 'pad':10},fontsize=10) 
+    plt.text(np.max(y_train)-2.2*np.std(y_train),np.min(y_train)+1.2*(np.std(y_train)),'Train:\nR2:%.3f \nMSE:%.3f\nRMSE:%.3f\nTest:\nR2:%.3f\nMSE:%.3f\nRMSE:%.3f' %(r2_score_train,mse_score_train,rmse_score_train,r2_score_test,mse_score_test,rmse_score_test), style='italic', bbox={'facecolor':'red', 'alpha':0.5, 'pad':10},fontsize=10) 
     plt.scatter(x=y_train,y=model_train,c='green',marker='o',label='train')
     plt.plot(x=y_train,y=y_train)
     plt.scatter(x=y_test,y=model_test,c='red',marker='o',label='test')
